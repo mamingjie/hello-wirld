827c2abf4122adffadc50fb293b0c8ca1664b18f
mmj34452 <mmj34452@ly.com>
2016-09-27 15:32:04 +0800

    export

--- src/main/java/com/ly/test/helloWorld/service/XlsxProcessor.java
+++ src/main/java/com/ly/test/helloWorld/service/XlsxProcessor.java
@@ -2,29 +2,29 @@ package com.ly.test.helloWorld.service;
 
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import com.ly.test.helloWorld.model.ExcelStorage;
 
 public class XlsxProcessor implements Runnable {
 
+	
+	private ExcelStorage excelStorage;
 	private Sheet sheet;
 	
-	//public static int SHEET_MAX_LINE = 1048576;
-	public static int SHEET_MAX_LINE = 104;
-	
-	public XlsxProcessor(Sheet sheet) {
+	public XlsxProcessor(ExcelStorage excelStorage, Sheet sheet) {
 		this.sheet = sheet;
+		this.excelStorage = excelStorage;
 	}
 	
 	@Override
 	public void run() {
-		for (int i=0; i < SHEET_MAX_LINE; i++) {
+		int total = excelStorage.getTotal();
+		for (int i=0; i < total; i++) {
 			Row row = sheet.createRow(i);
-			row.createCell(0).setCellValue("row" + (i+1) + "cell1");
-			row.createCell(1).setCellValue("row" + (i+1) + "cell2");
-			row.createCell(2).setCellValue("row" + (i+1) + "cell3");
-			row.createCell(3).setCellValue("row" + (i+1) + "cell4");
-			System.out.println(sheet.toString() + ": " + i);
+			row.createCell(0).setCellValue(i);
+			row.createCell(1).setCellValue("" + System.currentTimeMillis());
+			
+			excelStorage.setProgress(i+1);
 		}
-		ExcelService.onSheetComplete(sheet);
 	}
 
 }
